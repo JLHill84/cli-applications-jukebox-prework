6173e4b7a266cf437e98771e5b55ed7f7e8d29ed
Joshua Hill <josh@jlhworkshop.com>
2019-08-03 03:43:41 +0000

    Automatically backed up by Learn

--- lib/jukebox.rb
+++ lib/jukebox.rb
@@ -26,4 +26,6 @@ def help
  
  def play(songs)
    puts "Please enter a song name or number:"
-   ui = gets.strip
+   uInput = gets.strip
+   songs.each_with_index do |value, index|
+     if uInput = value
